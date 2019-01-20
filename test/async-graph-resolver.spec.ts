969aa8abba65d73cac33bcd2dcc36d3653912df4
Sela Marjan Almog <selama@wix.com>
2019-01-20 19:07:07 +0200

    reordering the tests

--- test/async-graph-resolver.spec.ts
+++ test/async-graph-resolver.spec.ts
@@ -2,66 +2,69 @@ import { expect } from 'chai';
 import { AsyncGraph } from '../src';
 
 describe('AsyncGraphResolver', () => {
-  it('should return non-aggregated data as an id -> value map', async () => {
-    const graph = new AsyncGraph();
+  describe('default aggregation', () => {
+    it('should resolve single node', async () => {
+      const graph = new AsyncGraph();
 
-    graph.addNode({
-      id: 'id1',
-      run: () => Promise.resolve(1),
-    });
-    expect(await graph.resolve()).to.deep.equal({
-      id1: 1,
-    });
-  });
-
-  it('should return non-aggregated data as an id -> value map when there is a dependency', async () => {
-    const graph = new AsyncGraph();
-
-    graph
-      .addNode({
+      graph.addNode({
         id: 'id1',
         run: () => Promise.resolve(1),
-      })
-      .addNode({
-        id: 'someId',
-        run: ({ id1 }) => Promise.resolve((id1 as number) + 999),
-        dependencies: ['id1'],
       });
-    expect(await graph.resolve()).to.deep.equal({
-      id1: 1,
-      someId: 1000,
+      expect(await graph.resolve()).to.deep.equal({
+        id1: 1,
+      });
     });
-  });
 
-  it('should return non-aggregated data as an id -> value map when there is a dependency', async () => {
-    const graph = new AsyncGraph();
+    it('should resovle 2 dependant nodes', async () => {
+      const graph = new AsyncGraph();
 
-    graph
-      .addNode({
-        id: 'id1',
-        run: () => Promise.resolve(0),
-      })
-      .addNode({
-        id: 'someId1',
-        run: ({ id1 }) => Promise.resolve((id1 as number) + 1),
-        dependencies: ['id1'],
-      })
-      .addNode({
-        id: 'someId2',
-        run: ({ id1 }) => Promise.resolve((id1 as number) + 2),
-        dependencies: ['id1'],
-      })
-      .addNode({
-        id: 'someId3',
-        run: ({ someId1, someId2 }) => Promise.resolve(`${someId1},${someId2}`),
-        dependencies: ['someId1', 'someId2'],
+      graph
+        .addNode({
+          id: 'id1',
+          run: () => Promise.resolve(1),
+        })
+        .addNode({
+          id: 'someId',
+          run: ({ id1 }) => Promise.resolve((id1 as number) + 999),
+          dependencies: ['id1'],
+        });
+      expect(await graph.resolve()).to.deep.equal({
+        id1: 1,
+        someId: 1000,
       });
+    });
 
-    expect(await graph.resolve()).to.deep.equal({
-      id1: 0,
-      someId1: 1,
-      someId2: 2,
-      someId3: '1,2',
+    it('should resolve rhombus shaped dependacies graph ', async () => {
+      const graph = new AsyncGraph();
+
+      graph
+        .addNode({
+          id: 'id1',
+          run: () => Promise.resolve(0),
+        })
+        .addNode({
+          id: 'someId1',
+          run: ({ id1 }) => Promise.resolve((id1 as number) + 1),
+          dependencies: ['id1'],
+        })
+        .addNode({
+          id: 'someId2',
+          run: ({ id1 }) => Promise.resolve((id1 as number) + 2),
+          dependencies: ['id1'],
+        })
+        .addNode({
+          id: 'someId3',
+          run: ({ someId1, someId2 }) =>
+            Promise.resolve(`${someId1},${someId2}`),
+          dependencies: ['someId1', 'someId2'],
+        });
+
+      expect(await graph.resolve()).to.deep.equal({
+        id1: 0,
+        someId1: 1,
+        someId2: 2,
+        someId3: '1,2',
+      });
     });
   });
 });
