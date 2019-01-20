4615a75ca649f199f8927c6fa2319c2a8e8a61f2
Sela Marjan Almog <selama@wix.com>
2019-01-20 19:40:09 +0200

    complex graph test was added

--- test/async-graph-resolver.spec.ts
+++ test/async-graph-resolver.spec.ts
@@ -66,5 +66,47 @@ describe('AsyncGraphResolver', () => {
         someId3: '1,2',
       });
     });
+
+    it('resolve a complex graph', async () => {
+      const graph = new AsyncGraph();
+
+      graph
+        .addNode({
+          id: 'id3',
+          run: () => Promise.resolve(3),
+        })
+        .addNode({
+          id: 'id5',
+          run: () => Promise.resolve(5),
+        })
+        .addNode({
+          id: 'id7',
+          run: () => Promise.resolve(7),
+        })
+        .addNode({
+          id: 'id3_5',
+          run: ({ id3, id5 }) => Promise.resolve(id3 * id5),
+          dependencies: ['id3', 'id5'],
+        })
+        .addNode({
+          id: 'id5_7',
+          run: ({ id5, id7 }) => Promise.resolve(id5 * id7),
+          dependencies: ['id5', 'id7'],
+        })
+        .addNode({
+          id: 'id3_5_7',
+          run: ({ id3, id5_7 }) => Promise.resolve(id3 * id5_7),
+          dependencies: ['id3', 'id5_7'],
+        });
+
+      expect(await graph.resolve()).to.deep.equal({
+        id3: 3,
+        id5: 5,
+        id7: 7,
+        id3_5: 15,
+        id5_7: 35,
+        id3_5_7: 105,
+      });
+    });
   });
 });
