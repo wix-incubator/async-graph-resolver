5559ee4327545a7060d357830850e8a6182076c0
Sela Marjan Almog <selama@wix.com>
2019-01-22 12:03:04 +0200

    Separate tests for invalid graph construction and graph resolving errors

---
+++ test/graph-execution-error-handling.spec.ts
@@ -0,0 +1,8 @@
+import { AsyncGraph } from '../src';
+import { expect } from 'chai';
+
+describe('error handling', () => {
+  describe('default error handling strategy: fail if any node execution fails', () => {
+    it('');
+  });
+});
